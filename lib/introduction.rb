7aedc2f1bf5b7fd8e8986ebf453d636426572ab0
Julie  McManus <julieannesays@gmail.com>
2020-01-21 23:56:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language
-  puts "Hi, my name is #{name}, and I speak #{english}."
+  puts "Hi, my name is #{name}, and I speak #{language}."
 end
